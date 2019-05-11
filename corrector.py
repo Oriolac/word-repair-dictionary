9caa91623f7ddff37e972000aafecc64c3901720
Oriol <oriolalascercos@gmail.com>
2019-05-11 13:34:16 +0200

    Oberts els altres fitxers i creat els altres

--- corrector.py
+++ corrector.py
@@ -4,9 +4,13 @@
 import sys
 
 def main():
-    DICCIONARI = open(sys.argv[1])
+    DICCIONARI = open(sys.argv[1], 'r')
+    TEXT_AMB_SORROLL = open(sys.argv[2], 'r')
     DICCIONARI = DICCIONARI.readline().split(' ')
+    TEXT_CORREGIT = open(sys.argv[3], 'w')
+    TEXT_NUM_EDICONS = open(sys.argv[4], 'w')
     
+
     print(DICCIONARI)
 
 if __name__ == "__main__":
