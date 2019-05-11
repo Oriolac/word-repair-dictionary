16f77a8d3e3b37be54445bfb4978bcd6db3fa8c5
Oriol <oriolalascercos@gmail.com>
2019-05-11 14:26:39 +0200

    Number of editions

--- corrector.py
+++ corrector.py
@@ -5,6 +5,16 @@
 
 import sys
 
+def number_of_editions(wrong_word, corrected_word, index):
+    if len(wrong_word) == index:
+        return corrected_word
+    return 0
+
+def buscar_paraula_correcta(wrong_word, diccionari):
+    """ Busca la paraula mes aproximada a wrong_word i retorna aquesta i el nombre d'edicions que s'han hagut de fer """
+    for corrected_word in diccionari:
+        print(wrong_word, corrected_word, number_of_editions(wrong_word, corrected_word, 0))
+
 def main():
     """ Corregeix el fitxer i retorna el text corregit i un altre text amb el nombre d'edicions """
 
@@ -17,7 +27,7 @@ def main():
     line = file_amb_soroll.readline().split(' ')[:-1]
     for word in line:
         if word not in diccionari:
-            print(word)
+            buscar_paraula_correcta(word, diccionari)
     
     file_diccionari.close()
     file_amb_soroll.close()
